a03cb3967197449f5a60d3b966e96feb4ee24851
Diego Maŕquez <desarrollo.marquez@gmail.com>
2019-01-08 23:44:54 -0500

    Adicion ajustes al modelo

--- ModelTrackerEntropia.sql
+++ ModelTrackerEntropia.sql
@@ -4,19 +4,22 @@
 --CREATE EXTENSION "pgcrypto";
 --SELECT gen_random_uuid()
 
+/*
+    *********************************** Model Data Tracker ***********************************
+*/
 
 DROP TABLE IF EXISTS  Usuario_Rol CASCADE;
 DROP TABLE IF EXISTS  Rol CASCADE;
 DROP TABLE IF EXISTS  Usuario CASCADE;
-DROP TABLE IF EXISTS  Persona CASCADE;
-DROP TABLE IF EXISTS  TipoDocumento CASCADE;
+DROP TABLE IF EXISTS  Vehiculo CASCADE;
+DROP TABLE IF EXISTS  TipoVehiculo CASCADE;
 
 
 /*
-    *********************************** Tabla TipoDocumento ***********************************
+    *********************************** Tabla TipoVehiculo ***********************************
 */
 
-CREATE TABLE TipoDocumento (
+CREATE TABLE TipoVehiculo (
   Codigo_id 	UUID DEFAULT gen_random_uuid(),
   Nombre    	VARCHAR NULL,
   Create_at 	TIMESTAMP DEFAULT CURRENT_TIMESTAMP(0),
@@ -26,20 +29,22 @@ CREATE TABLE TipoDocumento (
 
 
 /*
-    *********************************** Tabla Persona ***********************************
+    *********************************** Tabla Vehiculo ***********************************
 */
 
-CREATE TABLE Persona (
+CREATE TABLE Vehiculo (
   Codigo_id 		  UUID DEFAULT gen_random_uuid(),
-  NumeroId	          INTEGER      NULL,
-  Nombres                 VARCHAR(100) NOT NULL,
-  Apellidos               VARCHAR(100) NOT NULL,
+  Placa		          VARCHAR(100) NOT NULL UNIQUE,
+  Marca	                  VARCHAR(100) NOT NULL,
+  Modelo                  VARCHAR(100) NOT NULL,
+  CodigoInterno           VARCHAR(100) NOT NULL,
   Descripcion 		  TEXT         NULL,
-  TipoDocumento_id        UUID      NULL,
+  Estado     		  BOOLEAN DEFAULT false,
+  TipoVehiculo_id         UUID      NULL,
   Create_at               TIMESTAMP DEFAULT CURRENT_TIMESTAMP(0),
   Update_at               TIMESTAMP,
   PRIMARY KEY (Codigo_id),
-  CONSTRAINT fk_persona_tipodocumento FOREIGN KEY (TipoDocumento_id) REFERENCES TipoDocumento (Codigo_id) ON DELETE CASCADE
+  CONSTRAINT fk_vehiculo_tipovehiculo FOREIGN KEY (TipoVehiculo_id) REFERENCES TipoVehiculo (Codigo_id) ON DELETE CASCADE
 );
 
 /*
